d08f07d374a3579dfb8a5fddddca261cd108ff0c
Léonard Marques <lmarques@student.42.fr>
2016-10-18 00:29:48 +0200

    first version, everything seems to work just fine, but the get_next_line function is 1 line too long

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: lmarques <lmarques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/29 23:22:24 by lmarques          #+#    #+#             */
-/*   Updated: 2016/10/04 04:04:15 by lmarques         ###   ########.fr       */
+/*   Updated: 2016/10/18 00:27:50 by lmarques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char		*ft_realloc(char *str, int size)
 {
@@ -33,48 +34,101 @@ char		*ft_realloc(char *str, int size)
 	return (newstr);
 }
 
-int			ft_parse_buffer(char **str, char **line)
+char		*ft_set_tmp(char *tmp, char *buffer, int count)
 {
-	int	count;
+	if (!buffer)
+		return (NULL);
+	if (tmp)
+	{
+		tmp = (char *)malloc(sizeof(char) *
+			(ft_strlen(tmp) + ft_strlen(buffer) - count) + 1);
+		if (tmp)
+			tmp = ft_strcpy(tmp, buffer + count + 1);
+	}
+	else
+	{
+		tmp = (char *)malloc(sizeof(char) *
+			(ft_strlen(buffer) - count) + 1);
+		if (tmp)
+			tmp = ft_strcpy(tmp, buffer + count + 1);
+	}
+	return (tmp);
+}
+
+int			ft_parse_buffer(char **buffer, char **line, char **tmp)
+{
+	int		count;
 
 	count = 0;
-	while ((*str)[count])
+	while ((*buffer)[count])
 	{
-		if ((*str)[count] == '\n')
+		if ((*buffer)[count] == '\n')
 		{
-			*str = ft_realloc(*str, (ft_strlen(*str) - count) * -1);
-			*line = ft_realloc(*line, ft_strlen(*str));
-			*line = ft_strcat(*line, *str);
+			if (count < ft_strlen(*buffer))
+				*tmp = ft_set_tmp(*tmp, *buffer, count);
+			*buffer = ft_realloc(*buffer, (ft_strlen(*buffer) - count) * -1);
+			*line = ft_realloc(*line, ft_strlen(*buffer));
+			*line = ft_strcat(*line, *buffer);
 			return (1);
 		}
 		count++;
 	}
+	if (*tmp)
+		*tmp = (char *)malloc(sizeof(char) * ft_strlen(*tmp) + BUFF_SIZE + 1);
+	else
+		*tmp = (char *)malloc(sizeof(char) * BUFF_SIZE);
+	*tmp = ft_strcpy(*tmp, *buffer);
 	return (0);
 }
 
 int			get_next_line(int const fd, char **line)
 {
-	int		ret;
-	char	*buffer;
+	int			ret;
+	char		*buffer;
+	static char	*tmp = NULL;
 
-	ft_strclr(*line);
-	buffer = (char *)malloc(sizeof(*buffer) * BUFF_SIZE + 1);
-	if (!buffer)
+	*line = (char *)malloc(sizeof(char) * BUFF_SIZE);
+	buffer = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
+	if (!line || !buffer)
 		return (-1);
-	while ((ret = read(fd, buffer, BUFF_SIZE)))
+	ft_bzero(*line, BUFF_SIZE);
+	while (1)
 	{
+		ret = read(fd, buffer, BUFF_SIZE);
 		if (ret == -1)
 			return (-1);
 		buffer[ret] = '\0';
-		if (ft_parse_buffer(&buffer, line))
+		if (tmp && ft_strlen(tmp) != 0)
+		{
+			tmp = ft_realloc(tmp, ft_strlen(buffer));
+			buffer = ft_strcat(tmp, buffer);
+		}
+		if (ft_parse_buffer(&buffer, line, &tmp))
 			break ;
-		*line = ft_realloc(*line, ft_strlen(buffer));
-		*line = ft_strcat(*line, buffer);
-		if (ret != BUFF_SIZE)
-			return (1);
+		if (ft_strlen(tmp) == 0)
+			return (0);
 	}
-	if (ret == 0 || ret != BUFF_SIZE)
-		return (1);
-	free(buffer);
-	return (0);
+	return (1);
 }
+
+int main(int argc, const char *argv[])
+{
+	int		count;
+	int		fd;
+	int		tmp;
+	char	*line;
+
+	count = 0;
+	fd = open(argv[1], O_RDONLY);
+	tmp = 0;
+	while ((tmp = get_next_line(fd, &line)))
+	{
+		//printf("value : %d\n", tmp);
+		printf("line : %s\n", line);
+		count++;
+	}
+	//printf("value : %d\n", tmp);
+	argc++;
+	return 0;
+}
+
